17189ad90a2cb0166eedd87fcd84d34e030dcc19
victor06-git <asensiovector@gmail.com>
2024-10-17 18:04:11 +0200

    nou arxiu daus.py

--- Monopoly_proves.py
+++ Monopoly_proves.py
@@ -110,22 +110,7 @@ def banca():#Funció diners banca
 
 
 
-"""
-DADOS
-"""
 
-def daus(): #Funció daus: mostra el resultat aleatori dels dos daus
-    global resultat
-    dau_1()
-    dau_2()
-    suma = valor_dau_1 + valor_dau_2
-    resultat = print(f"Has tret {valor_dau_1} i {valor_dau_2} que suma: {suma}")
-def dau_1():#Funció dau 1
-    global valor_dau_1
-    valor_dau_1 = random.randint(1,6)
-def dau_2():#Funció dau 2
-    global valor_dau_2
-    valor_dau_2 = random.randint(1,6)
 
 
 
