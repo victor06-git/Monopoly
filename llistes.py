b113c59b0fbb62797aa2971a2fa782df9578bf60
victor06-git <asensiovector@gmail.com>
2024-10-21 15:36:17 +0200

    LISTES COMENTARI

--- llistes.py
+++ llistes.py
@@ -1,4 +1,12 @@
+"""
+PROPIETATS/CASES/HOTELS
+"""
+
 diners_propietat = [50, 60, 70, 80]
+
+
+
+
 tauler = ["Sortida","Lauria", "Rosselló", "Marina","Sort", "Consell de cent","Presó","Muntaner","Aribau","Caixa", "Sant Joan", "Aragó","Parking","Urquinaona", "Fontana","Sort", "Les Rambles", "Plaça Catalunya","Anar Presó","Portal de l'Àngel", "Via Augusta","Caixa", "Balmes", "Passeig de Gràcia"]
 cartes_caixa = ["Sortir de la presó","Anar a la presó","Error de la banca","Despeses mèdiques","Despeses escolars","Reparacions al carrer","Concurs de bellesa"]
 cartes_sort = ["Sortir de la presó","Anar a lla presó","Anar a la sortida","Anar tres espais endarrera","Fer reparacions a les propietats","Ets escollit alcalde"]
