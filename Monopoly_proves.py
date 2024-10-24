636cc52f2aca04ade37a6d26c8db13c25f24e1d0
victor06-git <asensiovector@gmail.com>
2024-10-24 17:44:24 +0200

    funcions torns jugador

--- Monopoly_proves.py
+++ Monopoly_proves.py
@@ -292,6 +292,13 @@ torn_actual = 0
 jugador_actual = torn(torn_actual)
 def torn_jugador_B():
     return jugador_actual == "Blau"
+def torn_jugador_V():
+    return jugador_actual == "Vermell"
+def torn_jugador_G():
+    return jugador_actual == "Groc"
+def torn_jugador_T():
+    return jugador_actual == "Taronja"
+
 def opcions(jugador_actual):
     opcions_jugadors = {
         "Vermell": jugador_v,
